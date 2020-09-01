03e38334b7380d0982d07d24673d019960d07008
Brad Caron <bacaron@iu.edu>
2020-09-01 16:01:19 -0400

    Update fslanalysis.sh

--- fslanalysis.sh
+++ fslanalysis.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-ndi=`jq -r '.ndi' config.json`
-odi=`jq -r '.odi' config.json`
-isovf=`jq -r '.isovf' config.json`
 ad=`jq -r '.ad' config.json`
 fa=`jq -r '.fa' config.json`
 md=`jq -r '.md' config.json`
 rd=`jq -r '.rd' config.json`
+ga=`jq -r '.ga' config.json`
+ak=`jq -r '.ak' config.json`
+mk=`jq -r '.mk' config.json`
+rk=`jq -r '.rk' config.json`
+ndi=`jq -r '.ndi' config.json`
+isovf=`jq -r '.isovf' config.json`
+odi=`jq -r '.odi' config.json`
 
 mkdir -p raw
 
 # set metrics for every situation
-if [ ! -f ${ndi} ]; then
-	metric="ad fa md rd"
-	echo ${metric}
-elif [ -f ${ndi} ] && [ -f ${fa} ]; then
-	metric="ndi odi isovf ad fa md rd"
-	echo ${metric}
+if [[ $fa == "null" ]];
+then
+	METRIC="ndi isovf odi"
+elif [[ $ndi == "null" ]] && [[ $ga == "null" ]]; then
+	METRIC="ad fa md rd"
+elif [[ $ndi == "null" ]] && [[ ! $ga == "null" ]]; then
+	METRIC="ad fa md rd ga ak mk rk"
+elif [[ ! $fa == "null" ]] && [[ ! $ndi == "null" ]] && [[ $ga == "null" ]]; then
+	METRIC="ad fa md rd ndi isovf odi"
 else
-	metric="ndi odi isovf"
-	echo ${metric}
+	METRIC="ad fa md rd ga ak mk rk ndi isovf odi"
 fi
 
 # extract tensor data in rois
